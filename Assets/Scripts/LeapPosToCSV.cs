787e1521687d64a50a4a5a87319074d2bf28ba86
KaroG97 <karola.graichen@st.ovgu.de>
2021-12-01 13:41:26 +0100

    add script write to csv

---
+++ Assets/Scripts/LeapPosToCSV.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class LeapPosToCSV : MonoBehaviour
+{
+    string filename = "/LeapHandPosition.csv";
+
+    [System.Serializable]
+
+    public class LeapHandPosition
+    {
+        public string point;
+        public float x;
+        public float y;
+        public float z;
+
+        public LeapHandPosition(string name, float tempX, float tempY, float tempZ){
+            point = name; 
+            x = tempX;
+            y = tempY;
+            z = tempZ;
+        }
+    }
+
+    [System.Serializable]
+
+    public class LeapHandPositionList
+    {
+        public LeapHandPosition[] positions;
+    }
+
+    public LeapHandPositionList currentList = new LeapHandPositionList();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        filename = Application.dataPath + filename;
+        print("Pfade " + filename);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float tempX = Random.Range(1.0f, 100.0f);
+        float tempY = Random.Range(1.0f, 100.0f);
+        float tempZ = Random.Range(1.0f, 100.0f);
+
+        LeapHandPosition newPosition = new LeapHandPosition ("temp", tempX, tempY, tempZ);
+        print("new " + newPosition.x);
+
+        System.Array.Resize(ref currentList.positions, currentList.positions.Length+1);
+           
+        currentList.positions[currentList.positions.Length-1] = newPosition;
+
+        if(Input.GetKey("p"))
+        {
+            writeCSV();
+        }
+    }
+
+    public void writeCSV()
+    {
+        print("Write CSV");
+        if(currentList.positions.Length > 0)
+        {
+            TextWriter tw = new StreamWriter(filename, false);
+            tw.WriteLine("Point ; X ; Y ; Z");
+            tw.Close();
+
+            tw = new StreamWriter(filename, true);
+            for(int i = 0; i < currentList.positions.Length; i++)
+            {
+                tw.WriteLine(currentList.positions[i].point + ";" +
+                             currentList.positions[i].x + ";"+
+                             currentList.positions[i].y + ";"+
+                             currentList.positions[i].z);
+            }
+            tw.Close();
+        }
+    }
+}
