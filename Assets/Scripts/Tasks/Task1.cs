2247640cc3bf245d75277e17791d40c671b64878
unknown <karola.graichen@st.ovgu.de>
2022-01-31 15:53:14 +0100

    init task1

---
+++ Assets/Scripts/Tasks/Task1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+* Task 1:
+* Objective: System activation
+* Components: Middle Part Sensor, Monitor, Hand
+* Preparation: Chose Left/Right Hand
+* Description: Ziel dieser Aufgabe ist es, das System "zu aktivieren". Dazu haelt der Proband seine Hand in die Mitte des Sensors, bis sich die Farbe des mittleren Bausteins aendert und ein Button auf dem 
+* Monitor erscheint. Der Proband fuerht nun eine Geste zum Druecken des Buttons aus. damit ist die Aufgabe abgeschlossen.
+*/
+
+public class Task1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
