74c33063261fa06795d379b44be36c4e04b27133
unknown <karola.graichen@st.ovgu.de>
2022-04-05 12:33:32 +0200

    calibrate room and sensor

---
+++ Assets/Scripts/CopyCat.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyCat : MonoBehaviour
+{
+    public GameObject Reference;
+    //public GameObject liege;
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        liege.transform.position = Reference.transform.position;
+        liege.transform.rotation = Reference.transform.rotation;
+        */
+        transform.position = Reference.transform.position;
+        transform.rotation = Reference.transform.rotation;
+    }
+    
+    void LateUpdate()
+    {
+        transform.position = Reference.transform.position;
+        transform.rotation = Reference.transform.rotation;
+    }
+    
+}
